8590a01987d3ed124a3c03627a1bac2ea059ae06
Shruti2004 <shrutikumbhar1122@gmail.com>
2025-03-26 21:08:24 +0530

    Changes made in Demo.java update m1()

--- Demo.java
+++ Demo.java
@@ -1,5 +1,8 @@
-public class Git{
+public class Demo{
     public static void main(String []args){
         System.out.println(" Shruti Kumbhar");
+        System.out.println(" now i am studying in CSE_TY in NKOCET SOLAPUR");
+        System.out.println("i have good knowedge of programming skills.");
+
     }
 }
